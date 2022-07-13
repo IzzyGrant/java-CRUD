6b5191416e4d2578b7ab4c5a17590a93677c6def
IzzyGrant <nikaylez@gmail.com>
2022-07-12 22:50:19 -0500

    Create README.md

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -12,7 +12,6 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! from maven");
     }
 }
-
